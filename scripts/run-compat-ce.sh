b5332be23c944355d5f6017dd20b29fe2373f96d
Tim Middleton <tim.middleton@oracle.com>
2022-01-19 13:19:05 +0800

    Update to CE 21.12.1.

--- scripts/run-compat-ce.sh
+++ scripts/run-compat-ce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 #
@@ -9,11 +9,11 @@
 # Run compatability tests
 set -e
 
-echo "Coherence CE 21.12"
-COHERENCE_VERSION=21.12 make clean build-test-images test-e2e-standalone
+echo "Coherence CE 21.12.1"
+COHERENCE_VERSION=21.12.1 make clean build-test-images test-e2e-standalone
 
-echo "Coherence CE 21.12 with Executor"
-PROFILES=,executor COHERENCE_VERSION=21.12 make clean build-test-images test-e2e-standalone
+echo "Coherence CE 21.12.1 with Executor"
+PROFILES=,executor COHERENCE_VERSION=21.12.1 make clean build-test-images test-e2e-standalone
 
 echo "Coherence CE 21.06.2"
 COHERENCE_VERSION=21.06.2 make clean build-test-images test-e2e-standalone
