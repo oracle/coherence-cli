27056cdf604b74ff759b4420eedfc5d9e1b9a2ff
Tim Middleton <tim.middleton@oracle.com>
2023-01-17 15:24:42 +0800

    Refactor formatting of table creation to support future formatting

--- pkg/cmd/federation.go
+++ pkg/cmd/federation.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
  */
@@ -432,7 +432,6 @@ service, type and participant. Specify -T to set type outgoing or incoming and -
 					}
 				}
 
-				// output
 				sb.WriteString(FormatFederationDetails(federationData, describeFederationType))
 			}
 
