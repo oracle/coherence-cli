bd5cbf465b567fd726bcb81b1b21ac645e463898
Tim Middleton <tim.middleton@oracle.com>
2023-03-24 08:17:34 +0800

    Fixup create clusters script

--- scripts/startup-clusters.sh
+++ scripts/startup-clusters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 #
@@ -24,6 +24,45 @@ PROFILE=""
 if [ "$COHERENCE_GROUP_ID" == "com.oracle.coherence" ] ; then
    PROFILE="-P commercial"
 fi
+
+# Add a default ~/.m2/settings.xml to ensure we can get the snapshot versions
+cat > ~/.m2/settings.xml <<EOF
+<settings>
+  <profiles>
+    <profile>
+      <id>default</id>
+      <repositories>
+        <repository>
+          <id>ossrh-staging</id>
+          <name>OSS Sonatype Staging</name>
+          <url>https://oss.sonatype.org/content/groups/staging/</url>
+          <snapshots>
+            <enabled>false</enabled>
+          </snapshots>
+          <releases>
+            <enabled>true</enabled>
+          </releases>
+        </repository>
+
+        <repository>
+          <id>snapshots-repo</id>
+          <url>https://oss.sonatype.org/content/repositories/snapshots</url>
+          <releases>
+            <enabled>false</enabled>
+          </releases>
+          <snapshots>
+            <enabled>true</enabled>
+          </snapshots>
+        </repository>
+      </repositories>
+    </profile>
+  </profiles>
+
+  <activeProfiles>
+    <activeProfile>default</activeProfile>
+  </activeProfiles>
+</settings>
+EOF
 
 COHERENCE_STARTUP="-Dcoherence.wka=127.0.0.1 -Dcoherence.ttl=0 -Dcoherence.clusterport=${CLUSTER_PORT} -D${TAG} -Dcoherence.management.http=all -Dcoherence.management.http.port=0"
 
