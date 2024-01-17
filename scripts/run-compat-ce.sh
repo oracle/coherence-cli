daa96a8042add7a29510603732e01b5df6a684cd
Tim Middleton <tim.middleton@oracle.com>
2024-01-17 08:29:06 +0800

    Add new ce versions to CI/CD (#149)

--- scripts/run-compat-ce.sh
+++ scripts/run-compat-ce.sh
@@ -9,8 +9,8 @@
 # Run compatability tests
 set -e
 
-echo "Coherence CE 22.06.6"
-COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=22.06.6 make clean build-test-images test-e2e-standalone
+echo "Coherence CE 22.06.7"
+COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=22.06.7 make clean build-test-images test-e2e-standalone
 
 echo "Coherence CE 23.09.1"
 COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=23.09.1 make clean build-test-images test-e2e-standalone
@@ -18,8 +18,8 @@ COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=23.09.1 make cle
 echo "Coherence CE 23.09.1 with Executor"
 COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 PROFILES=,executor COHERENCE_VERSION=23.09.1 make clean build-test-images test-e2e-standalone
 
-echo "Coherence CE 14.1.1-0-15"
-COHERENCE_VERSION=14.1.1-0-15 make clean build-test-images test-e2e-standalone
+echo "Coherence CE 14.1.1-0-16"
+COHERENCE_VERSION=14.1.1-0-16 make clean build-test-images test-e2e-standalone
 
 echo "Coherence CE 23.09.1 with Topics"
 COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 PROFILES=,topics COHERENCE_VERSION=23.09.1 make clean build-test-images test-e2e-topics
