0d5b9d1c2ea78405ef97ec4108e52f00d0780457
Github Action <tmiddlet2666@users.noreply.github.com>
2024-12-20 04:44:02 +0000

    Release Coherence CLI version: 1.8.0

---
+++ docs/1.8.0/pages/docs/reference/01_overview.js
@@ -0,0 +1,315 @@
+<doc-view>
+
+<h2 id="_reference">Reference</h2>
+<div class="section">
+<p>The Coherence CLI contains many commands to manage and monitor Coherence clusters.</p>
+
+<v-layout row wrap class="mb-5">
+<v-flex xs12>
+<v-container fluid grid-list-md class="pa-0">
+<v-layout row wrap class="pillars">
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/05_clusters"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Clusters</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Manage Cluster Connections.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/10_contexts"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Contexts</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Manage cluster connections you are working with.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/15_members"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Members</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe member information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/17_machines"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Machines</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe machine information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/20_services"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Services</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe service information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/25_caches"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Caches</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe cache information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/26_view_caches"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">View Caches</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe view cache information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/30_topics"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Topics</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display Topics information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/40_persistence"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Persistence</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and perform Persistence operations.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/42_federation"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Federation</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and perform Federation operations.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/50_proxies"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Proxy Servers</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe proxy server information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/55_http_servers"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Http Proxy Servers</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe Http proxy server information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/56_elastic_data"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Elastic Data</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe Elastic Data information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/58_http_sessions"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Http Sessions</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe Coherence*Web Http session information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/60_executors"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Executors</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and describe Executors.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/66_reporters"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Reporters</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display and manage Coherence reporters.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/85_diagnostics"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Diagnostics</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Take multiple thread dumps, heap dumps or JFR&#8217;s across members.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/90_health"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Health</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Display health information.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/45_nslookup"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">NS Lookup</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Look up various Name Service endpoints for a cluster.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/92_reset"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Reset MBean Statistics</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Reset MBean Statistics for various resources.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/95_misc"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Miscellaneous</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Various settings related commands.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/98_create_clusters"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Create Clusters</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Experimental command to create development clusters.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/reference/99_monitor_clusters"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Monitor Clusters</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Experimental command to monitor clusters with text UI.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+</v-container>
+</v-flex>
+</v-layout>
+</div>
+</doc-view>
